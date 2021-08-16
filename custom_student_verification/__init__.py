2f38d1a0eb3437bf96208eac33c2a7c0064d45d7
Alfred Chavez <alfred@bitmaker.la>
2021-08-16 13:16:52 -0500

    feat: [BB-4655] update version

--- custom_student_verification/__init__.py
+++ custom_student_verification/__init__.py
@@ -4,6 +4,6 @@ Custom Django app for student verification.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '1.2.4'
+__version__ = '1.3.5'
 
 default_app_config = 'custom_student_verification.apps.CustomStudentVerificationConfig'  # pylint: disable=invalid-name
